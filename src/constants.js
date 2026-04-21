915a295796637e91eac05ec5f337182e48f0200a
Puja Chaudhary <102856633+pujatech08@users.noreply.github.com>
2026-04-21 10:07:41 +0530

    Update API key placeholder in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyBA_Papf7hX2r4NJqhEXE-LEEewYnZYGOw";
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=Enter your key";
 
